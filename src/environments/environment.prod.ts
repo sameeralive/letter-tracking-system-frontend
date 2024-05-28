f073373d833b675b2036bccdbf1de715259dc5c6
sameera <sameeraherathlive@gmail.com>
2024-05-28 20:02:40 +0530

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,10 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-export let root_url = 'http://127.0.0.1:8000/api/';
-export let treatment_doc_path = 'http://127.0.0.1:8000/public/treatment-docs/';
+export let root_url = 'http://localhost/letter-registry-backend/api/';
+export let treatment_doc_path = 'http://localhost/letter-registry-backend/public/treatment-docs/';
 
 export const environment = {
+  baseUrl: 'http://localhost/letter-registry-backend/',
   production: true,
 };
